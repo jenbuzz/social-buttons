3ff3f353dd62d324351fcbbbc6215ec0a10ff1d7
dan-lyn <daniel_lynge@hotmail.com>
2014-02-19 16:48:55 +0100

    dynamically add buttons with js

--- social-buttons.js
+++ social-buttons.js
@@ -1,1 +1,28 @@
-// TODO
+$.fn.socialButtons = function(options) {
+  var config = {
+    "facebook": {
+      "shortname": "f",
+      "class": "social-facebook"
+    },
+    "twitter": {
+      "shortname": "t",
+      "class": "social-twitter"
+    },
+    "googleplus": {
+      "shortname": "g+",
+      "class": "social-googleplus"
+    }
+  }
+
+  var container = $(this);
+
+  container.append("<ul></ul>");
+
+  $.each(options, function(index, value) {
+    if (config.hasOwnProperty(value)) {
+      $("<li>"+config[value].shortname+"</li>").appendTo(container.find("ul")).on("click", function() {
+        console.log("click on "+value);
+      });
+    }
+  });
+}
