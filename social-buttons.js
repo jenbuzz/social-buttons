21f9c1a80e770390f953698bff74a226da0d8525
dan-lyn <daniel_lynge@hotmail.com>
2014-02-23 10:26:23 +0100

    adding options.url to shareurl in config

--- social-buttons.js
+++ social-buttons.js
@@ -3,17 +3,17 @@ $.fn.socialButtons = function(options) {
     "facebook": {
       "shortname": "f",
       "class": "social-facebook",
-      "shareurl": "https://www.facebook.com/sharer/sharer.php?u="
+      "shareurl": "https://www.facebook.com/sharer/sharer.php?u="+options.url
     },
     "twitter": {
       "shortname": "t",
       "class": "social-twitter",
-      "shareurl": "http://twitter.com/share?text="+options.text+"&url="
+      "shareurl": "http://twitter.com/share?text="+options.text+"&url="+options.url
     },
     "googleplus": {
       "shortname": "g+",
       "class": "social-googleplus",
-      "shareurl": "https://plus.google.com/share?url="
+      "shareurl": "https://plus.google.com/share?url="+options.url
     }
   }
 
@@ -24,7 +24,7 @@ $.fn.socialButtons = function(options) {
   $.each(options.socialNetworks, function(index, value) {
     if (config.hasOwnProperty(value)) {
       $("<li class=\""+config[value].class+"\"></li>").appendTo(container.find("ul")).on("click", function() {
-        window.open(config[value].shareurl+options.url, '', 'menubar=no,toolbar=no,resizeable=no,scrollbars=no,height=600,width=600,top='+((screen.height/2)-300)+',left='+((screen.width/2)-300));
+        window.open(config[value].shareurl, '', 'menubar=no,toolbar=no,resizeable=no,scrollbars=no,height=600,width=600,top='+((screen.height/2)-300)+',left='+((screen.width/2)-300));
       });
     }
   });
