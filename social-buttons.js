2cb543e9391da0bf4315262691a4a014a18dc4bf
dan-lyn <daniel_lynge@hotmail.com>
2014-02-25 10:41:37 +0100

    pass jquery through immediate function

--- social-buttons.js
+++ social-buttons.js
@@ -1,40 +1,42 @@
-$.fn.socialButtons = function(options) {
-  this.options = {
-    socialNetworks: [],
-    url: "",
-    text: "",
-    sharelabel: true
-  };
-  $.extend(this.options, options);
-  this.options.url = encodeURIComponent(this.options.url);
-  this.options.text = encodeURIComponent(this.options.text);
+(function($) {
+  $.fn.socialButtons = function(options) {
+    this.options = {
+      socialNetworks: [],
+      url: "",
+      text: "",
+      sharelabel: true
+    };
+    $.extend(this.options, options);
+    this.options.url = encodeURIComponent(this.options.url);
+    this.options.text = encodeURIComponent(this.options.text);
   
-  var config = {
-    facebook: {
-      cssclass: "social-facebook",
-      shareurl: "https://www.facebook.com/sharer/sharer.php?u="+this.options.url
-    },
-    twitter: {
-      cssclass: "social-twitter",
-      shareurl: "http://twitter.com/share?text="+this.options.text+"&url="+this.options.url
-    },
-    googleplus: {
-      cssclass: "social-googleplus",
-      shareurl: "https://plus.google.com/share?url="+this.options.url
+    var config = {
+      facebook: {
+        cssclass: "social-facebook",
+        shareurl: "https://www.facebook.com/sharer/sharer.php?u="+this.options.url
+      },
+      twitter: {
+        cssclass: "social-twitter",
+        shareurl: "http://twitter.com/share?text="+this.options.text+"&url="+this.options.url
+      },
+      googleplus: {
+        cssclass: "social-googleplus",
+        shareurl: "https://plus.google.com/share?url="+this.options.url
+      }
     }
-  }
 
-  var container = $(this);
-  if (this.options.sharelabel) {
-    container.append("<label>SHARE</label>");
-  }
-  container.append("<ul></ul><div></div>");
-
-  $.each(this.options.socialNetworks, function(index, value) {
-    if (config.hasOwnProperty(value)) {
-      $("<li class=\""+config[value].cssclass+"\"></li>").appendTo(container.find("ul")).on("click", function() {
-        window.open(config[value].shareurl, '', 'menubar=no,toolbar=no,resizeable=no,scrollbars=no,height=600,width=600,top='+((screen.height/2)-300)+',left='+((screen.width/2)-300));
-      });
+    var container = $(this);
+    if (this.options.sharelabel) {
+      container.append("<label>SHARE</label>");
     }
-  });
-}
+    container.append("<ul></ul><div></div>");
+
+    $.each(this.options.socialNetworks, function(index, value) {
+      if (config.hasOwnProperty(value)) {
+        $("<li class=\""+config[value].cssclass+"\"></li>").appendTo(container.find("ul")).on("click", function() {
+          window.open(config[value].shareurl, '', 'menubar=no,toolbar=no,resizeable=no,scrollbars=no,height=600,width=600,top='+((screen.height/2)-300)+',left='+((screen.width/2)-300));
+        });
+      }
+    });
+  }
+})(jQuery);
