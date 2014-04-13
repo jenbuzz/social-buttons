0ec89b206bda7ac1e59d885726e8a6e60b8f6ce1
dan-lyn <daniel_lynge@hotmail.com>
2014-04-13 12:58:28 +0200

    options and socialnetworks as local variables (removed that=this)

--- social-buttons-wide.js
+++ social-buttons-wide.js
@@ -2,44 +2,42 @@
   "use strict";
 
   $.fn.socialButtonsWide = function() {
-    var that = this;
-
-    that.options = {
+    var options = {
       url: "",
       text: "",
     };
-    if (that.options.url=="" && window.location.href!=undefined) {
-      that.options.url = window.location.href;
+    if (options.url=="" && window.location.href!=undefined) {
+      options.url = window.location.href;
     }
-    if (that.options.text=="" && document.title!="") {
-      that.options.text = document.title;
+    if (options.text=="" && document.title!="") {
+      options.text = document.title;
     }
-    that.options.url = encodeURIComponent(that.options.url);
-    that.options.text = encodeURIComponent(that.options.text);
+    options.url = encodeURIComponent(options.url);
+    options.text = encodeURIComponent(options.text);
 
-    that.socialNetworks = {
+    var socialNetworks = {
       facebook: {
         title: "Share on Facebook",
         cssclass: "social-facebook",
-        shareurl: "https://www.facebook.com/sharer/sharer.php?u="+that.options.url,
+        shareurl: "https://www.facebook.com/sharer/sharer.php?u="+options.url,
         height: 600,
         width: 600
       },
       twitter: {
         title: "Share on Twitter",
         cssclass: "social-twitter",
-        shareurl: "http://twitter.com/share?text="+that.options.text+"&url="+that.options.url,
+        shareurl: "http://twitter.com/share?text="+options.text+"&url="+options.url,
         height: 600,
         width: 600
       }
     };
 
-    var container = $(that);
+    var container = $(this);
 
     var htmlUl = "<ul></ul><div></div>";
     container.append(htmlUl);
 
-    $.each(that.socialNetworks, function(index, value) {
+    $.each(socialNetworks, function(index, value) {
         $("<li class=\""+value.cssclass+"\" title=\""+value.title+"\"></li>").appendTo(container.find("ul")).on("click", function() {
           window.open(value.shareurl, '', 'menubar=no,toolbar=no,resizeable=no,scrollbars=no,height='+value.height+',width='+value.width+',top='+((screen.height/2)-(value.height/2))+',left='+((screen.width/2)-(value.width/2)));
         });
