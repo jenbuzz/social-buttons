fdd85f6e8d8d72ae40e0cf0bf41aa56564ae3854
dan-lyn <daniel_lynge@hotmail.com>
2016-04-09 19:22:32 +0200

    using single quotes as default

--- social-buttons-fa/social-buttons-fa.js
+++ social-buttons-fa/social-buttons-fa.js
@@ -1,94 +1,94 @@
 (function($) {
-  "use strict";
+  'use strict';
 
   $.fn.socialButtonsFa = function(customMode, customOptions, customSocialNetworks) {
     var that = this;
     var options = customOptions || {};
     var socialNetworks = customSocialNetworks || {};
 
-    var mode = (customMode!==undefined && (customMode==="share" || customMode==="wide" || customMode==="pages")) ? customMode : "share";
+    var mode = (customMode!==undefined && (customMode==='share' || customMode==='wide' || customMode==='pages')) ? customMode : 'share';
 
-    if (mode==="wide") {
+    if (mode==='wide') {
       if (window.location.href!==undefined && document.title!==undefined) {
         that.options = {
-          socialNetworks: ["facebook", "twitter"],
+          socialNetworks: ['facebook', 'twitter'],
           url: encodeURIComponent(window.location.href),
           text: encodeURIComponent(document.title)
         };
       } else {
         return;
       }
-    } else if (mode==="pages") {
+    } else if (mode==='pages') {
       that.options = {
-        target: "_blank"
+        target: '_blank'
       };
       $.extend(that.options, options);
     } else {
       that.options = {
-        socialNetworks: ["facebook", "twitter", "googleplus"],
-        url: "",
-        text: "",
+        socialNetworks: ['facebook', 'twitter', 'googleplus'],
+        url: '',
+        text: '',
         sharelabel: true,
-        sharelabelText: "SHARE",
+        sharelabelText: 'SHARE',
         verticalAlign: false
       };
       $.extend(that.options, options);
-      if (that.options.url==="" && $("link[rel=canonical]").length && $("link[rel=canonical]").attr('href').indexOf("://") != -1) {
-        that.options.url = $("link[rel=canonical]").attr('href');
-      } else if (that.options.url==="" && !$("link[rel=canonical]").length && window.location.href!==undefined) {
+      if (that.options.url==='' && $('link[rel=canonical]').length && $('link[rel=canonical]').attr('href').indexOf('://') != -1) {
+        that.options.url = $('link[rel=canonical]').attr('href');
+      } else if (that.options.url==='' && !$('link[rel=canonical]').length && window.location.href!==undefined) {
         that.options.url = window.location.href;
       }
-      if (that.options.text==="" && document.title!=="") {
+      if (that.options.text==='' && document.title!=='') {
         that.options.text = document.title;
       }
       that.options.url = encodeURIComponent(that.options.url);
       that.options.text = encodeURIComponent(that.options.text);
     }
 
-    if (mode==="pages") {
+    if (mode==='pages') {
       that.socialNetworks = {
         facebook: {
-          title: "Visit us on Facebook",
-          cssclass: "social-facebook",
-          cssclassfa: "fa fa-facebook",
-          pageurl: ""
+          title: 'Visit us on Facebook',
+          cssclass: 'social-facebook',
+          cssclassfa: 'fa fa-facebook',
+          pageurl: ''
         },
         twitter: {
-          title: "Visit us on Twitter",
-          cssclass: "social-twitter",
-          cssclassfa: "fa fa-twitter",
-          pageurl: ""
+          title: 'Visit us on Twitter',
+          cssclass: 'social-twitter',
+          cssclassfa: 'fa fa-twitter',
+          pageurl: ''
         },
         googleplus: {
-          title: "Visit us on Google+",
-          cssclass: "social-googleplus",
-          cssclassfa: "fa fa-google-plus",
-          pageurl: ""
+          title: 'Visit us on Google+',
+          cssclass: 'social-googleplus',
+          cssclassfa: 'fa fa-google-plus',
+          pageurl: ''
         }
       };
     } else {
       that.socialNetworks = {
         facebook: {
-          title: "Share on Facebook",
-          cssclass: "social-facebook",
-          cssclassfa: "fa fa-facebook",
-          shareurl: "https://www.facebook.com/sharer/sharer.php?u=" + that.options.url,
+          title: 'Share on Facebook',
+          cssclass: 'social-facebook',
+          cssclassfa: 'fa fa-facebook',
+          shareurl: 'https://www.facebook.com/sharer/sharer.php?u=' + that.options.url,
           height: 600,
           width: 600
         },
         twitter: {
-          title: "Share on Twitter",
-          cssclass: "social-twitter",
-          cssclassfa: "fa fa-twitter",
-          shareurl: "https://twitter.com/intent/tweet?text=" + that.options.text + "&url=" + that.options.url,
+          title: 'Share on Twitter',
+          cssclass: 'social-twitter',
+          cssclassfa: 'fa fa-twitter',
+          shareurl: 'https://twitter.com/intent/tweet?text=' + that.options.text + '&url=' + that.options.url,
           height: 600,
           width: 600
         },
         googleplus: {
-          title: "Share on Google+",
-          cssclass: "social-googleplus",
-          cssclassfa: "fa fa-google-plus",
-          shareurl: "https://plus.google.com/share?url=" + that.options.url,
+          title: 'Share on Google+',
+          cssclass: 'social-googleplus',
+          cssclassfa: 'fa fa-google-plus',
+          shareurl: 'https://plus.google.com/share?url=' + that.options.url,
           height: 600,
           width: 600
         }
@@ -101,14 +101,14 @@
     });
 
     var container = $(this);
-    if (mode==="wide") {
-      var ulWide = "<ul class=\"social-wide\"></ul><div class=\"clearfix\"></div>";
+    if (mode==='wide') {
+      var ulWide = '<ul class="social-wide"></ul><div class="clearfix"></div>';
       container.append(ulWide);
 
       $.each(that.socialNetworks, function(index, value) {
         if (that.options.socialNetworks.indexOf(index)>=0) {
-          var liWide = "<li class=\"" + value.cssclass + " " + value.cssclassfa + "\" title=\"" + value.title + "\"><span>" + value.title + "</span></li>";
-          $(liWide).appendTo(container.find("ul")).on("click", function() {
+          var liWide = '<li class="' + value.cssclass + ' ' + value.cssclassfa + '" title="' + value.title + '"><span>' + value.title + '</span></li>';
+          $(liWide).appendTo(container.find('ul')).on('click', function() {
             var wideHeight = value.height;
             var wideWidth = value.width;
             var wideTop = ((screen.height/2)-(value.height/2));
@@ -118,14 +118,14 @@
           });
         }
       });
-    } else if (mode==="pages") {
-      var ulPages = "<ul class=\"social-pages\"></ul><div class=\"clearfix\"></div>";
+    } else if (mode==='pages') {
+      var ulPages = '<ul class="social-pages"></ul><div class="clearfix"></div>';
       container.append(ulPages);
 
       $.each(that.socialNetworks, function(index, value) {
         if (value.pageurl!=='') {
-          var liPages = "<li class=\"" + value.cssclass + "\" title=\"" + value.title + "\"><div class=\"" + value.cssclassfa + "\"></div></li>";
-          $(liPages).appendTo(container.find("ul")).on("click", function() {
+          var liPages = '<li class="' + value.cssclass + '" title="' + value.title + '"><div class="' + value.cssclassfa + '"></div></li>';
+          $(liPages).appendTo(container.find('ul')).on('click', function() {
             window.open(value.pageurl, that.options.target);
           });
         }
@@ -135,21 +135,21 @@
         var verticalAlign = (that.options.verticalAlign!==undefined) ? that.options.verticalAlign : false;
 
         if (that.options.sharelabel) {
-          var classVerticalLabel = (verticalAlign) ? " class=\"vertical vertical-label\"" : "";
-          var htmlLabel = "<label" + classVerticalLabel + ">" + that.options.sharelabelText + "</label>";
+          var classVerticalLabel = (verticalAlign) ? ' class="vertical vertical-label"' : '';
+          var htmlLabel = '<label' + classVerticalLabel + '>' + that.options.sharelabelText + '</label>';
           container.append(htmlLabel);
         }
 
-        var classVertical = (verticalAlign) ? " vertical vertical-ul" : "";
-        var ulShare = "<ul class=\"social-share" + classVertical + "\"></ul><div></div>";
+        var classVertical = (verticalAlign) ? ' vertical vertical-ul' : '';
+        var ulShare = '<ul class="social-share' + classVertical + '"></ul><div></div>';
         container.append(ulShare);
 
         $.each(that.options.socialNetworks, function(index, value) {
           if (that.socialNetworks.hasOwnProperty(value)) {
-            var classVertical = (verticalAlign) ? " vertical" : "";
+            var classVertical = (verticalAlign) ? ' vertical' : '';
 
-            var liShare = "<li class=\"" + that.socialNetworks[value].cssclass+classVertical + "\" title=\"" + that.socialNetworks[value].title + "\"><div class=\"" + that.socialNetworks[value].cssclassfa + "\"></div></li>";
-            $(liShare).appendTo(container.find("ul")).on("click", function() {
+            var liShare = '<li class="' + that.socialNetworks[value].cssclass+classVertical + '" title="' + that.socialNetworks[value].title + '"><div class="' + that.socialNetworks[value].cssclassfa + '"></div></li>';
+            $(liShare).appendTo(container.find('ul')).on('click', function() {
               var shareHeight = that.socialNetworks[value].height;
               var shareWidth = that.socialNetworks[value].width;
               var shareTop = ((screen.height/2)-(that.socialNetworks[value].height/2));
--- social-buttons-share/social-buttons-share.js
+++ social-buttons-share/social-buttons-share.js
@@ -1,5 +1,5 @@
 (function($) {
-  "use strict";
+  'use strict';
 
   $.fn.socialButtonsShare = function(customOptions, customSocialNetworks) {
     var that = this;
@@ -7,20 +7,20 @@
     var socialNetworks = customSocialNetworks || {};
   
     that.options = {
-      socialNetworks: ["facebook", "twitter", "googleplus"],
-      url: "",
-      text: "",
+      socialNetworks: ['facebook', 'twitter', 'googleplus'],
+      url: '',
+      text: '',
       sharelabel: true,
-      sharelabelText: "SHARE",
+      sharelabelText: 'SHARE',
       verticalAlign: false
     };
     $.extend(that.options, options);
-    if (that.options.url==="" && $("link[rel=canonical]").length && $("link[rel=canonical]").attr('href').indexOf("://") != -1) {
-      that.options.url = $("link[rel=canonical]").attr('href');
-    } else if (that.options.url==="" && !$("link[rel=canonical]").length && window.location.href!==undefined) {
+    if (that.options.url==='' && $('link[rel=canonical]').length && $('link[rel=canonical]').attr('href').indexOf('://') != -1) {
+      that.options.url = $('link[rel=canonical]').attr('href');
+    } else if (that.options.url==="" && !$('link[rel=canonical]').length && window.location.href!==undefined) {
       that.options.url = window.location.href;
     }
-    if (that.options.text==="" && document.title!=="") {
+    if (that.options.text==='' && document.title!=='') {
       that.options.text = document.title;
     }
     that.options.url = encodeURIComponent(that.options.url);
@@ -28,23 +28,23 @@
   
     that.socialNetworks = {
       facebook: {
-        title: "Share on Facebook",
-        cssclass: "social-facebook",
-        shareurl: "https://www.facebook.com/sharer/sharer.php?u=" + that.options.url,
+        title: 'Share on Facebook',
+        cssclass: 'social-facebook',
+        shareurl: 'https://www.facebook.com/sharer/sharer.php?u=' + that.options.url,
         height: 600,
         width: 600
       },
       twitter: {
-        title: "Share on Twitter",
-        cssclass: "social-twitter",
-        shareurl: "https://twitter.com/intent/tweet?text=" + that.options.text + "&url=" + that.options.url,
+        title: 'Share on Twitter',
+        cssclass: 'social-twitter',
+        shareurl: 'https://twitter.com/intent/tweet?text=' + that.options.text + '&url=' + that.options.url,
         height: 600,
         width: 600
       },
       googleplus: {
-        title: "Share on Google+",
-        cssclass: "social-googleplus",
-        shareurl: "https://plus.google.com/share?url=" + that.options.url,
+        title: 'Share on Google+',
+        cssclass: 'social-googleplus',
+        shareurl: 'https://plus.google.com/share?url=' + that.options.url,
         height: 600,
         width: 600
       }
@@ -60,27 +60,27 @@
       var verticalAlign = that.options.verticalAlign;
 
       if (that.options.sharelabel) {
-        var classVerticalLabel = (verticalAlign) ? " class=\"vertical vertical-label\"" : "";
-        var htmlLabel = "<label" + classVerticalLabel + ">" + that.options.sharelabelText + "</label>";
+        var classVerticalLabel = (verticalAlign) ? ' class="vertical vertical-label"' : '';
+        var htmlLabel = '<label' + classVerticalLabel + '>' + that.options.sharelabelText + '</label>';
         container.append(htmlLabel);
       }
 
-      var classVertical = (verticalAlign) ? " class=\"vertical vertical-ul\"" : "";
-      var htmlUl = "<ul" + classVertical + "></ul><div></div>";
+      var classVertical = (verticalAlign) ? ' class="vertical vertical-ul"' : '';
+      var htmlUl = '<ul' + classVertical + '></ul><div></div>';
       container.append(htmlUl);
 
       $.each(that.options.socialNetworks, function(index, value) {
         if (that.socialNetworks.hasOwnProperty(value)) {
-          var classVertical = (verticalAlign) ? " vertical" : "";
+          var classVertical = (verticalAlign) ? ' vertical' : '';
 
-          var liShare = "<li class=\"" + that.socialNetworks[value].cssclass + classVertical + "\" title=\"" + that.socialNetworks[value].title + "\"></li>";
+          var liShare = '<li class="' + that.socialNetworks[value].cssclass + classVertical + '" title="' + that.socialNetworks[value].title + '"></li>';
 
           var shareHeight = that.socialNetworks[value].height;
           var shareWidth = that.socialNetworks[value].width;
           var shareTop = ((screen.height/2) - (that.socialNetworks[value].height/2));
           var shareLeft = ((screen.width/2)-(that.socialNetworks[value].width/2))
 
-          $(liShare).appendTo(container.find("ul")).on("click", function() {
+          $(liShare).appendTo(container.find('ul')).on('click', function() {
             window.open(that.socialNetworks[value].shareurl, '', 'menubar=no,toolbar=no,resizeable=no,scrollbars=no,height=' + shareHeight + ',width=' + shareWidth + ',top=' + shareTop + ',left=' + shareLeft);
           });
         }
